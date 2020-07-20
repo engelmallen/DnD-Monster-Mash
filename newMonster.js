411421e5f39fa54ab615d0380796ac72f71411a7
Engel Mallen <emallen@uarts.edu>
2020-07-20 00:22:05 -0400

    character search working

--- newMonster.js
+++ newMonster.js
@@ -1,50 +1,42 @@
+//Identifiers:
 let search = document.getElementById('search')
-// let searchBy = document.getElementById('searchBy')
 let searchBtn = document.getElementById('searchBtn')
-
-searchBtn.addEventListener('click', function () {
-    getInfo()
-})
-
+let youAreSearching = document.getElementById('youAreSearching')
 
 /*variables:*/
 let getCriteriaBtn = ""
+let curInfo = []
+
+// Buttons:
+searchBtn.addEventListener('click', function () {
+        if (getCriteriaBtn != "") {
+        getInfo()
+        }
+    })
+
+// Ajax Functionalities:
+function getInfo(criteria) {
+
+    let settings = {
+        "async"         : true,
+        "crossDomain"   : true,
+        "url"           : "https://api.open5e.com/"+ getCriteriaBtn + search.value,
+        "method"        : "GET",
+    }
+
+    $.ajax(settings).done(function (response) {
+        curInfo.push(response)
+        console.log(curInfo)
+        })
+    }
+
 
 function getCriteria() {
     let searchBy = document.getElementsByClassName('searchBy')
     for (var i = 0; i < searchBy.length; i++) {
         searchBy[i].addEventListener('click', function (e) {
-            getCriteriaBtn = "/" + e.target.name    
+            getCriteriaBtn = e.target.name
+            youAreSearching.innerHTML = "I'm looking for this " + e.target.value + ":"   
         })
     }
 }getCriteria()
-
-function getInfo(criteria) {
-
-criteria = getCriteriaBtn
-// https://api.open5e.com/monsters/?search=fir 
-// let spells =        "spells/?"
-// let monsters =      "monsters/?"
-// let documents =     "documents/?"
-// let backgrounds =   "backgrounds/?"
-// let planes =        "planes/?"
-// let feats =         "feats/?"
-// let conditions =    "conditions/?"
-// let races =         "races/?"
-// let classes =       "classes/?"
-// let magicitems =    "magicitems/?"
-// let weapons =       "weapons/?"
-// let search =        "search/?"
-
-let settings = {
-    "async"         : true,
-    "crossDomain"   : true,
-    "url"           : "https://api.open5e.com"+ getCriteriaBtn + "/?search="+search.value,
-    "method"        : "GET",
-}
-
-$.ajax(settings).done(function (response) {
-    console.log(response)
-    })
-}
-
